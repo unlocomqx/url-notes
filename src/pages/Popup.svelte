5ca17c2a00f650e7727ed55635ae0147dff6ae79
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-03-16 12:37:08 +0100

    add pell

--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -2,6 +2,7 @@
   import Icon from "@iconify/svelte"
   import {type Context, db} from "../db"
   import {liveQuery} from "dexie"
+  import { exec, init } from 'pell'
 
   let context = $state<Context>('page')
 
@@ -19,8 +20,22 @@
   )
   let notes = $derived(filterNotes(context))
 
-  $inspect($notes)
+  function pell(node: HTMLDivElement, { id, content }:{id: number, content: string}) {
+    const editor = init({
+      element: node,
+      onChange: (html: string) => {
+        db.notes.update(id, { content: html })
+      },
+      actions:['bold','link','italic','underline']
+    })
+
+    editor.content.innerHTML = content
+  }
 </script>
+
+<svelte:head>
+  <link rel="stylesheet" type="text/css" href="https://unpkg.com/pell/dist/pell.min.css">
+</svelte:head>
 
 <div class="p-2 flex flex-col gap-2">
   <div class="tabs tabs-border" role="tablist">
@@ -31,8 +46,9 @@
 
   <div class="notes flex flex-col gap-2 min-h-16 border-2 border-base-300 rounded-lg p-4">
     {#each $notes as {id, content} (id)}
-      <div class="p-2 border-2 border-base-300 rounded-lg" contenteditable="true">
-        {content}
+      <div class="p-2 border-2 border-base-300 rounded-lg"
+           use:pell={{ id, content }}
+      >
       </div>
     {/each}
   </div>
