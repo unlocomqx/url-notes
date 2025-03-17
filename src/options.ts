07b8818d23c3d3863f2a96ee9dcda499bd9aeaa4
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-03-17 20:42:03 +0100

    Add options page

---
+++ src/options.ts
@@ -0,0 +1,4 @@
+import Options from './pages/Options.svelte'
+import { mount } from "svelte";
+
+mount(Options, { target: document.body });
---
+++ src/pages/Options.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte"
+  import {type Context, db} from "../db"
+  import {liveQuery} from "dexie"
+  import Editor from "../lib/Editor.svelte"
+  import browser from "webextension-polyfill"
+  import {onMount} from "svelte"
+  import ThemeController from "../lib/ThemeController.svelte"
+
+  let context = $state<Context>('page')
+  let context_url = $state<string>('')
+
+  async function addNote() {
+    let url: string | undefined = ''
+
+    const tab = (await browser.tabs.query({active: true, currentWindow: true}))[0]
+
+    if (context === 'page') {
+      if (tab.url) {
+        const page_url = new URL(tab.url)
+        url = getPageUrl(page_url)
+      }
+    } else if (context === 'website') {
+      if (tab.url) {
+        const page_url = new URL(tab.url)
+        url = page_url.origin
+      }
+    }
+
+    if (!url) {
+      url = ''
+    }
+
+    const id = await db.notes.add({
+      context,
+      content: '',
+      url
+    })
+
+    console.log(id)
+  }
+
+  let filterNotes = (context: Context, context_url: string) => liveQuery(
+    () => {
+      let url = ''
+      if (context === 'global') {
+        url = ''
+      } else if (context === 'page') {
+        if (context_url) {
+          const page_url = new URL(context_url)
+          url = getPageUrl(page_url)
+        }
+      } else if (context === 'website') {
+        if (context_url) {
+          const page_url = new URL(context_url)
+          url = page_url.origin
+        }
+      }
+
+      return db.notes
+        .where({context, url})
+        .toArray()
+    }
+  )
+  let notes = $derived(filterNotes(context, context_url))
+
+  function getPageUrl(url: URL) {
+    let without_hash = url.href.replace(url.hash, '')
+    return without_hash.replace(/#$/, '')
+  }
+
+  onMount(() => {
+    browser.tabs.query({active: true, currentWindow: true})
+      .then((tabs) => {
+        const tab = tabs[0]
+        context_url = tab.url || ''
+        const url = new URL(context_url)
+        let stored_contexts = localStorage.getItem('context')
+        let contexts = JSON.parse(stored_contexts || '{}')
+        if (contexts[url.origin]) {
+          context = contexts[url.origin]
+        } else {
+          context = 'page'
+        }
+      })
+  })
+
+  $effect(() => {
+    if (context_url) {
+      const url = new URL(context_url)
+      let stored_contexts = localStorage.getItem('context')
+      let contexts = JSON.parse(stored_contexts || '{}')
+      contexts[url.origin] = context
+      localStorage.setItem('context', JSON.stringify(contexts))
+    }
+  })
+</script>
+
+<div class="p-1 flex flex-col gap-2">
+  <div class="flex justify-between items-center">
+    <div class="tabs tabs-border" role="tablist">
+      <input aria-label="Page" bind:group={context} class="tab" name="context" type="radio" value="page"/>
+      <input aria-label="Website" bind:group={context} class="tab" name="context" type="radio" value="website"/>
+      <input aria-label="Global" bind:group={context} class="tab" name="context" type="radio" value="global"/>
+    </div>
+    <ThemeController/>
+  </div>
+
+  <div class="notes flex flex-col gap-2">
+    {#each $notes as {id, content} (id)}
+      <div class="group relative border-2 border-base-300 rounded-lg">
+        <Editor {id} {content} onchange={(content) => {
+          db.notes.update(id, {content})
+        }}/>
+
+        <div class="absolute top-0 right-0 p-1 opacity-0 group-hover:opacity-100">
+          <button class="btn btn-xs btn-error"
+                  onclick={() => {
+                      if(true || confirm('Are you sure?')) {
+                        db.notes.delete(id)
+                      }
+                  }}
+          >
+            <Icon icon="ic:baseline-delete"/>
+          </button>
+        </div>
+      </div>
+    {/each}
+  </div>
+
+  <div class="p-2">
+    <button class="btn btn-primary btn-sm" onclick={addNote}>
+      <Icon icon="ic:add"/>
+      {#if context === 'page'}
+        New page note
+      {:else if context === 'website'}
+        New website note
+      {:else}
+        New global note
+      {/if}
+    </button>
+  </div>
+</div>
+
+<style>
+
+</style>
