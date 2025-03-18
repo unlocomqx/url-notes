20f6d29c42a5ef0137961a4a124056a468138340
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-03-18 07:54:41 +0100

    Save light / dark theme

--- src/lib/ThemeController.svelte
+++ src/lib/ThemeController.svelte
@@ -1,26 +1,50 @@
 <script lang="ts">
   import {LocalStore} from "./persist.svelte"
+  import Icon from "@iconify/svelte"
+  import {onMount} from "svelte"
 
   let themes = [
-    {name: 'Light', value: 'light'},
-    {name: 'Silk', value: 'silk'},
-    {name: 'Dark', value: 'dark'},
-    {name: 'Retro', value: 'retro'},
-    {name: 'Garden', value: 'garden'},
-    {name: 'Forest', value: 'forest'},
-    {name: 'Pastel', value: 'pastel'},
-    {name: 'Dracula', value: 'dracula'},
-    {name: 'Acid', value: 'acid'},
-    {name: 'Night', value: 'night'},
-    {name: 'Dim', value: 'dim'},
-    {name: 'Abyss', value: 'abyss'},
-    {name: 'Nord', value: 'nord'},
-    {name: 'Sunset', value: 'sunset'},
+    {name: 'Light', value: 'light', type: 'light'},
+    {name: 'Silk', value: 'silk', type: 'light'},
+    {name: 'Retro', value: 'retro', type: 'light'},
+    {name: 'Garden', value: 'garden', type: 'light'},
+    {name: 'Pastel', value: 'pastel', type: 'light'},
+    {name: 'Acid', value: 'acid', type: 'light'},
+    {name: 'Nord', value: 'nord', type: 'light'},
+    {name: 'Dark', value: 'dark', type: 'dark'},
+    {name: 'Forest', value: 'forest', type: 'dark'},
+    {name: 'Dracula', value: 'dracula', type: 'dark'},
+    {name: 'Night', value: 'night', type: 'dark'},
+    {name: 'Dim', value: 'dim', type: 'dark'},
+    {name: 'Abyss', value: 'abyss', type: 'dark'},
+    {name: 'Sunset', value: 'sunset', type: 'dark'},
   ]
   const current_theme = new LocalStore<string>('current_theme', 'light')
+  const dark_light = new LocalStore<{ light: string, dark: string }>('dark_light', {
+    light: 'light',
+    dark: 'dark',
+  })
 
   $effect(() => {
     document.documentElement.dataset.theme = current_theme.value
+  })
+
+  onMount(() => {
+    // detect light / dark mode
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      current_theme.value = dark_light.value.dark
+    } else {
+      current_theme.value = dark_light.value.light
+    }
+
+    // add event listener
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+      if (e.matches) {
+        current_theme.value = dark_light.value.dark
+      } else {
+        current_theme.value = dark_light.value.light
+      }
+    })
   })
 </script>
 
@@ -38,7 +62,7 @@
   </div>
   <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
   <ul class="dropdown-content bg-base-300 rounded-box z-1 w-48 overflow-auto p-2 shadow-2xl" tabindex="0">
-    {#each themes as {name, value}}
+    {#each themes as {name, value, type}}
       <li>
         <label
             class="btn btn-sm btn-ghost grow flex items-center gap-2"
@@ -48,6 +72,13 @@
           <input
               aria-label="{name}"
               bind:group={current_theme.value}
+              onchange={() => {
+                if(type === 'light') {
+                  dark_light.value.light = value
+                } else {
+                  dark_light.value.dark = value
+                }
+              }}
               class="theme-controller hidden"
               name="theme-dropdown"
               type="radio"
@@ -62,7 +93,14 @@
               <div class="bg-accent w-2 h-6"></div>
             </div>
           </div>
-          <span class="text-left grow">{name}</span>
+          <span class="text-left grow flex gap-2 items-center">
+            {#if type === 'light'}
+              <Icon icon="ic:outline-wb-sunny"></Icon>
+              {:else}
+              <Icon icon="ic:outline-dark-mode"></Icon>
+            {/if}
+            {name}
+          </span>
         </label>
       </li>
     {/each}
